755640fd24c63ce28af37e394d54f0e6c4a8b434
heavenshell <heavenshell.jp@gmail.com>
2014-01-25 11:04:02 +0900

    Add not generate file test.

--- tests/test_autodoc.py
+++ tests/test_autodoc.py
@@ -20,6 +20,7 @@ from tests.app import create_app
 root_path = os.path.dirname(os.path.abspath(__file__))
 var_path = os.path.join(root_path, 'var')
 
+os.environ['PYAUTODOC'] = '1'
 
 class TestAutodoc(TestCase):
     def setUp(self):
@@ -190,6 +191,32 @@ class TestAutodocGenerate(TestCase):
     def test_post(self):
         """ POST / """
         res = self.client.post_json('/', params={'id': 1, 'message': 'foo'})
+        self.assertEqual(res.status_code, 200)
+
+        return res
+
+class TestAutodocNotGenerate(TestCase):
+    def setUp(self):
+        app = create_app
+        self.client = TestApp(app)
+
+        if os.path.exists(var_path):
+            shutil.rmtree(var_path)
+            os.mkdir(var_path)
+
+    @autodoc.generate(
+        os.path.join(var_path, 'test_generate.md'),
+        template=os.path.join(root_path, 'templates/markdown.md'))
+    def tearDownClass(cls):
+        ret = os.path.exists(os.path.join(var_path, 'test_generate.md'))
+        assert ret is False
+
+    @autodoc.describe('GET /')
+    def test_should_not_generate(self):
+        """ Should not generate when PYAUTODOC environ is not exists. """
+        del os.environ['PYAUTODOC']
+
+        res = self.client.get('/')
         self.assertEqual(res.status_code, 200)
 
         return res
