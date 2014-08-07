7f0396963dcd8690a4db24cc06d186a43853b1b1
heavenshell <heavenshell.jp@gmail.com>
2014-08-08 01:19:53 +0900

    Add tests.

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -88,3 +88,43 @@ class TestRequestsResponse(TestCase):
         }
         for k, v in iteritems(autodoc.vars[0]):
             self.assertEqual(v, var[k])
+
+        autodoc.clear()
+
+    def test_parse_responses(self):
+        """ Should stack responses. """
+        headers = {'content-type': 'application/json'}
+        req = self.create_request(url='http://localhost:5000/',
+                                  method='GET',
+                                  headers=headers)
+
+        res = self.send(req, '', 'data/get.json')
+        autodoc.parse('GET /', res)
+        autodoc.parse('GET /', res)
+        var = {
+            'response_content_type': 'application/json',
+            'response_body': '{"response": "index"}\n',
+            'describe': 'GET /',
+            'request': 'GET /',
+            'params': {},
+            'status_code': 200,
+            'target_url': 'http://localhost:5000/',
+            'describe_separators': '====='
+        }
+        vars = [var, var]
+        self.assertEqual(autodoc.vars, vars)
+        autodoc.clear()
+
+    def test_clear_responses(self):
+        """ Should clear stacked WebTest responses. """
+        headers = {'content-type': 'application/json'}
+        req = self.create_request(url='http://localhost:5000/',
+                                  method='GET',
+                                  headers=headers)
+
+        res = self.send(req, '', 'data/get.json')
+
+        autodoc.parse('GET /', res)
+        autodoc.parse('GET /', res)
+        autodoc.clear()
+        self.assertEqual(autodoc.vars, [])
